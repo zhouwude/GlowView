61a10b4d95c77a3fbe209b4c6ebe04043f7e930a
YouXianMing <705786299@qq.com>
2015-07-05 23:17:28 +0800

    1.0

--- GlowView/ViewController.m
+++ GlowView/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "UIView+GlowView.h"
 
 @interface ViewController ()
 
@@ -16,12 +17,47 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+    
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    {
+        UILabel *label  = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 50)];
+        label.text      = @"YouXianMing";
+        label.textColor = [UIColor blackColor];
+        label.font      = [UIFont fontWithName:@"AppleSDGothicNeo-Thin" size:20.f];
+        
+        label.center    = self.view.center;
+        [self.view addSubview:label];
+        
+        label.glowRadius        = @(4.f);
+        label.glowOpacity       = @(1.f);
+        label.glowColor         = [UIColor redColor];
+        
+        label.glowDuration      = @(1.f);
+        label.hideDuration      = @(0.5f);
+        label.glowAnimationDuration = @(1.f);
+        
+        [label createGlowLayer];
+        [label insertGlowLayer];
+        [label startGlowLoop];
+    }
+    
+    {
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(90, 350, 19, 30)];
+        imageView.image        = [UIImage imageNamed:@"demo"];
+        [self.view addSubview:imageView];
+        
+        imageView.glowRadius        = @(2.f);
+        imageView.glowOpacity       = @(0.5f);
+        imageView.glowColor         = [UIColor cyanColor];
+        
+        imageView.glowDuration      = @(1.f);
+        imageView.hideDuration      = @(0.5f);
+        imageView.glowAnimationDuration = @(1.f);
+        
+        [imageView createGlowLayer];
+        [imageView insertGlowLayer];
+        [imageView startGlowLoop];
+    }
 }
 
 @end
